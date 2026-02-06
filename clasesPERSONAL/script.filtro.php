002d9306b9d124d0301ed8962a8823b7e5afef8d
fatima554477 <fatima-arellano@hotmail.com>
2026-02-05 22:33:37 -0600

    Add files via upload

--- clasesPERSONAL/script.filtro.php
+++ clasesPERSONAL/script.filtro.php
@@ -34,6 +34,7 @@
 
 <script type="text/javascript">
 	
+/////////////////PARA AUTORIZAR BONO VYO////////////////////////////////////
 function pasara1_personalVYO(pasara1_personalVYO_id){
 
 	var checkBox = document.getElementById("VYO"+pasara1_personalVYO_id);
@@ -44,7 +45,7 @@ function pasara1_personalVYO(pasara1_personalVYO_id){
 	pasapersonalVYO_text = "no";
 	}
 	  $.ajax({
-		url:'BONOS/controladorAE.php',
+		url:'calendariodeeventos2/controladorAE.php',
 		method:'POST',
 		data:{pasara1_personalVYO_id:pasara1_personalVYO_id,pasapersonalVYO_text:pasapersonalVYO_text},
 		beforeSend:function(){
@@ -52,7 +53,7 @@ function pasara1_personalVYO(pasara1_personalVYO_id){
 	},
 		success:function(data){
 			
-		load(1);	
+	//$("#reset_personal").load(location.href + " #reset_personal");			
 			
 		$('#mensajePERSONAL').html("<span id='ACTUALIZADO' >"+data+"</span>").fadeIn().delay(2000).fadeOut();
 	}
@@ -70,7 +71,7 @@ function pasara1_personalDIRECCION(pasara1_personalDIRECCION_id){
 	pasapersonalDIRECCION_text = "no";
 	}
 	  $.ajax({
-		url:'BONOS/controladorAE.php',
+		url:'calendariodeeventos2/controladorAE.php',
 		method:'POST',
 		data:{pasara1_personalDIRECCION_id:pasara1_personalDIRECCION_id,pasapersonalDIRECCION_text:pasapersonalDIRECCION_text},
 		beforeSend:function(){
@@ -96,7 +97,7 @@ function pasara1_personalADMIN(pasara1_personalADMIN_id){
 	pasapersonalADMIN_text = "no";
 	}
 	  $.ajax({
-		url:'BONOS/controladorAE.php',
+		url:'calendariodeeventos2/controladorAE.php',
 		method:'POST',
 		data:{pasara1_personalADMIN_id:pasara1_personalADMIN_id,pasapersonalADMIN_text:pasapersonalADMIN_text},
 		beforeSend:function(){
